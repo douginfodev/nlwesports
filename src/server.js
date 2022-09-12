21d7941eb07a824000cccf710b87c252ca964119
Douglas Ramos <47486673+douginfodev@users.noreply.github.com>
2022-09-12 18:28:21 -0300

    Server End Aula 1
    Server end lesson 1

---
+++ src/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express();
+//rotas GET
+app.get('/ads', (request, response) => {
+    response.json([
+        { id: 1, name: 'nome 1' },
+        { id: 2, name: 'nome 5' },
+        { id: 3, name: 'nome 3' },
+    ]);
+});
+//escutar a porta do local host
+app.listen(3333);
