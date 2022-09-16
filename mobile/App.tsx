f426a126b85837f8c60f2eb59829ac94c39ded17
Douglas Ramos <47486673+douginfodev@users.noreply.github.com>
2022-09-16 00:22:05 -0300

    Splash Screen
    Splash Screen Mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Background from './src/components/Background';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Mobile React</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Background>
+      
+    </Background>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
