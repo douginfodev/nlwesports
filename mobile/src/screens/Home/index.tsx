9f448b9e79b9b6b0906aaa2355d7e5b346c8d8bb
Douglas Ramos <47486673+douginfodev@users.noreply.github.com>
2022-09-17 00:23:51 -0300

    Project Logo
    Adding Project Logo NLW ESPORTS

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,8 +1,17 @@
+import React from 'react';;
+import { View, Image } from 'react-native';
+
+import logoImg from '../../assets/logo-nlw-esports.png'
 import { styles } from './styles';
 
 export default function Home() {
   return (
-    
+    <View style={styles.container}> 
+        <Image 
+        source={logoImg}
+        style={styles.logo}
+        />
+    </View>    
   );
 }
 
---
+++ mobile/src/screens/Home/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+import { THEME } from "../../theme";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    logo:{
+        width: 214,
+        height: 120,
+        marginTop: ,
+        marginBottom: 48,
+    }
+
+});
